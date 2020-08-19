7b19d6db9a7a67555528881b91787367c350a0c7
Michael Manning <michaelmanning390@gmail.com>
2020-08-18 23:43:53 -0700

    More progress on client, removed components, added connection input etc.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,74 +1,97 @@
 <template>
     <div id="app" class="container m-auto center">
         <div>
-            <h2 class="text-red-400 strong mt-2 text-center">
+            <h2 class="title text-red-400 strong mt-2 text-center text-6xl">
                 Number Horde
             </h2>
-            <ConnectInput v-if="connection === null" />
-            <FireInput v-if="connection !== null" />
+            <div class="text-center" v-if="connection === null">
+                Welcome to Number Horde!<br />
+                <p class="text">
+                    To join a server, punch in the IP and Port below!
+                </p>
+                <label class="block">
+                    <div>Server IP:</div>
+                    <input type="text" name="ip" class="rounded-l h-6 text-black p-2 text-center" v-model="ipData" v-on:keyup.enter="connect">
+                    <button class="p-1 h-7 bg-blue-500 hover:bg-blue-900 rounded-r" type="submit" v-on:click="connect">Connect</button>
+                </label>
+            </div>
+            <div class="text-center" v-if="connection !== null">
+                <input v-model="fireData" class="rounded-l w-3/5 h-6 center my-6 mx-auto p-2 text-center text-black center" type="text" v-on:keyup.enter="fire">
+                <button class="bg-red-500 hover:bg-red-900 rounded-r" type="submit" v-on:click="fire">Fire!</button>
+                <div class="h-20 w-20 rounded" v-bind:style="charStyle" v-if="!!charStyle">&nbsp;</div>
+            </div>
         </div>
     </div>
 </template>
 <script>
-import ConnectInput from './components/ConnectInput';
-import FireInput from './components/FireInput';
-
 export default {
     name: "App",
-    components: {
-        ConnectInput,
-        FireInput
-    },
     data() {
         return {
             connection: null,
+            ipData: "localhost:9080",
+            fireData: "",
+            charStyle: undefined
         };
     },
     created() {
-        console.log("Starting connection to WebSocket Server");
-        this.connection = new WebSocket("ws://localhost:9080");
-        this.connection.onmessage = async (e) => {
-            const encoded = await new Response(e.data).text();
-            const pkt = JSON.parse(encoded);
-
-            console.log("Received", pkt, pkt.data);
-
-            this.sendMessage("Good day sir!");
-            this.connection.close(1000, "Deliberate disconnection");
-        }
-        this.connection.onopen = (e) => {
-            console.log(e);
-            console.log("Successfully connected to the server!");
-        }
-        this.connection.onerror = (e) => {
-            console.log("Error", e);
-        }
-        this.connection.onclose = (e) => {
-            console.log("Connection closed", e);
-            this.connection = null;
-        }
+        this.connect();
     },
     methods: {
         connect() {
+            console.log("Starting connection to WebSocket Server");
+            this.connection = new WebSocket("ws://" + this.ipData);
+            this.connection.onmessage = async (e) => {
+                const encoded = await new Response(e.data).text();
+                const pkt = JSON.parse(encoded);
 
+                console.log("Received", pkt.method, pkt.data);
+                this.charStyle = {
+                    'background-color': "#" + pkt.data.color
+                };
+            }
+            this.connection.onopen = (e) => {
+                console.log(e);
+                console.log("Successfully connected to the server!");
+            }
+            this.connection.onerror = (e) => {
+                console.log("Error", e);
+            }
+            this.connection.onclose = (e) => {
+                console.log("Connection closed", e);
+                this.connection = null;
+            }
         },
-        sendMessage(message) {
-            console.log("Sent:", message);
-            this.connection.send(this.createPkt("simple", message));
+        fire() {
+            this.sendMessage("fire", { "shot": this.fireData });
+            this.fireData = "";
         },
-        createPkt(method, message) {
-            return JSON.stringify({
+        sendMessage(method, data = {}) {
+            let pkt = {
                 "method": method,
-                "data": {
-                    "msg": message
-                }
-            })
-        }
+                "data": data
+            };
+            console.log("Sent", pkt);
+            this.connection.send(JSON.stringify(pkt));
+        },
     }
 };
 </script>
 <style>
 body {
     @apply text-white bg-black;
+}
+
+.title {
+    font-family: 'Buried';
+}
+
+@font-face {
+    font-family: 'Buried';
+    src: url("./fonts/BuriedExtended.eot");
+    src:
+        url("./fonts/BuriedExtended.woff") format("woff"),
+        url("./fonts/BuriedExtended.otf") format("opentype");
+    /*url("./fonts/BuriedExteneded.svg#filename") format("svg");*/
 }
 </style>
--- client/src/components/FireInput.vue
+++ client/src/components/FireInput.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="text-center">
-        <input class="rounded-l w-3/5 center my-6 mx-auto p-2 text-center text-black center" type="text">
+        <input v-bind="inputData" class="rounded-l w-3/5 center my-6 mx-auto p-2 text-center text-black center" type="text">
         <button type="bg-red-500 hover:bg-red-700 rounded-r">Fire!</button>
     </div>
 </template>
 <script>
 export default {
-    name: 'FireInput'
+    name: 'FireInput',
+    data() {
+        inputData: ""
+    },
+    methods: {
+        onFire(event) {
+            this.$emit('submit', this.inputdata);
+        }
+    }
 };
 </script>
 <style scoped>
