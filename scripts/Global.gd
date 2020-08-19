9aa52995ac4578e8e40d217368952f52d38b9be0
Michael Manning <michaelmanning390@gmail.com>
2020-08-18 18:14:44 -0700

    Light refactoring around scripts and the server

---
+++ scripts/Global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var node_creation_parent = null
+
+func instance_node(node, location, parent):
+	var node_instance = node.instance()
+	parent.add_child(node_instance)
+	node_instance.global_position = location
+	return node_instance
+
+func random_color():
+	return Color.from_hsv(randf(), 1, 1)
